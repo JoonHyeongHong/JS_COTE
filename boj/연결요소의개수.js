39274cd9b8c0764dd493103600a6cd9ceb593f00
JoonHyeongHong <jshwa0429@gmail.com>
2023-07-03 20:18:04 +0900

    7월 3일 문제 풀이#4

---
+++ boj/연결요소의개수.js
@@ -0,0 +1,37 @@
+const input = require("fs")
+  .readFileSync("/home/sesa/JS_Cote/boj/example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [NM, ...edges] = input;
+const [N, M] = NM.split(" ").map(Number);
+
+const graph = Array.from({ length: N + 1 }, () => []);
+edges.forEach((edge) => {
+  const [start, end] = edge.split(" ").map(Number);
+  graph[start].push(end);
+  graph[end].push(start);
+});
+
+const visited = new Array(N + 1).fill(0);
+
+const dfs = (node) => {
+  visited[node] = 1;
+  for (const index of graph[node]) {
+    if (!visited[index]) {
+      dfs(index);
+    }
+  }
+};
+
+let count = 0;
+
+for (let i = 1; i <= N; i++) {
+  if (!visited[i]) {
+    dfs(i);
+    count++;
+  }
+}
+
+console.log(count);
